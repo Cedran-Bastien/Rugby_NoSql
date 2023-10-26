9460055f2ff3be8eb3c62c5842386ada317b0dae
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2023-10-26 12:18:06 +0200

    Implement question a/ and b/

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Main {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Queris qr = new Queris();
+
+        boolean end = false;
+        while (!end) {
+
+            // Ask command
+            String command = sc.nextLine();
+
+            // Get Arguments
+            String[] argument = command.split(" ");
+            // Delete too much space
+            for (int i = 0; i <argument.length; i++){
+                argument[i] = argument[i] .trim();
+            }
+
+            switch (argument[0]){
+                case "quit":
+                    end = true;
+                    break;
+                case "getEquipe":
+//                  .. [nom equipe]
+                    qr.getJoueur(argument[1]);
+                    break;
+                case "getEquipeDatePoints" :
+//                    .. ["date at format : '01/02/2003'"] , [Points]
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.FRENCH);
+                    try {
+                        Date date = formatter.parse(argument[1]);
+                        qr.getMatchDatePoints(date, Integer.parseInt(argument[2]));
+                    } catch (ParseException e) {
+                        System.out.println("Error : wrong date argument : must be in form : '01/02/2003' or date given doesn't exist");
+                        break;
+                    } catch (NumberFormatException n){
+                        System.out.println("value of point isn't number, please check the given value");
+                    }
+                    break;
+            }
+        }
+    }
 }
--- src/Queris.java
+++ src/Queris.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class Queris {
+import Pojo.Joueurs;
+import Pojo.Match;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Queris {
+
+//    MongoClient mg ;
+    MongoDatabase md;
+
+    MongoCollection<Match> matchs;
+
+    public Queris(){
+        // Connect to database
+        // Get database
+        // Get matchs collection
+        matchs = Connection.getMatchsCollection();
+    }
+
+    public void getJoueur(String idEquipe){
+        // Get team player
+        List<Joueurs> correspondance = new ArrayList<>();
+        for (Match m : matchs.find()){
+            if (!m.getEquipeRecoit().getidEquipe().equals(idEquipe) || m.getEquipeRecu().getidEquipe().equals(idEquipe)){
+                for (Joueurs joueurs : m.getJoueurs()){
+                    if (Objects.equals(joueurs.getIdEquipe(), idEquipe))
+                        correspondance.add(joueurs);
+                }
+            }
+        }
+
+        // Sort list
+        correspondance.sort((j1, j2) -> {
+            int res = 0;
+            if (j1.getCoefficientEfficacite() < j2.getCoefficientEfficacite())
+                res = 1;
+
+            if (j1.getCoefficientEfficacite() > j2.getCoefficientEfficacite())
+                res = -1;
+
+            return res;
+        });
+
+        for (Joueurs j : correspondance){
+            System.out.printf("\nnom : %s : " +
+                    "\n\t - temps de jeu : %s, " +
+                    "\n\t - Nb essais marqués : %s, " +
+                    "\n\t - Nb point marqué : %s, " +
+                    "\n\t - Coefficient d'éfficacité : %s",
+                    j.getNom(),
+                    j.getTempsJeu(),
+                    j.getNbEssaisMarques(),
+                    j.getPtsMarques(),
+                    j.getCoefficientEfficacite());
+        }
+
+    }
+
+    public void getMatchDatePoints(Date d, int points){
+        for (Match m : matchs.find()){
+            if (m.getDate().equals(d) && m.getPtsD() > points || m.getPtsE() > points){
+                // todo -> amelioré match print
+                System.out.println(m.getIdMatch());
+            }
+        }
+    }
 }
